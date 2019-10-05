d54d2c090321f20a5c124dbf63032867a49e21f5
Ralph L <lettau.ralph@gmail.com>
2019-10-05 12:14:10 +0200

    Impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/de/qmsq/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package de.qmsq.sfgpetclinic.services.map;
+
+import de.qmsq.sfgpetclinic.model.Vet;
+import de.qmsq.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
