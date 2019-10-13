1d0db0336c684c092e1cd318cf5a19d143721ed7
Ralph L <lettau.ralph@gmail.com>
2019-10-13 20:48:25 +0200

    Added Specialties Map Service Closes #31

---
+++ pet-clinic-data/src/main/java/de/qmsq/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package de.qmsq.sfgpetclinic.services;
+
+import de.qmsq.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/qmsq/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,34 @@
+package de.qmsq.sfgpetclinic.services.map;
+
+import de.qmsq.sfgpetclinic.model.Specialty;
+import de.qmsq.sfgpetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
