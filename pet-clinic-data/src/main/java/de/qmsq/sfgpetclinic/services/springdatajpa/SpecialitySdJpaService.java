556b93311b4ceaaa1a96eba0bdf9bf499c8e0f88
Ralph L <lettau.ralph@gmail.com>
2019-10-18 17:38:04 +0200

    Created Speciality spring data jpa repository. Closes #44

---
+++ pet-clinic-data/src/main/java/de/qmsq/sfgpetclinic/services/springdatajpa/SpecialitySdJpaService.java
@@ -0,0 +1,48 @@
+package de.qmsq.sfgpetclinic.services.springdatajpa;
+
+import de.qmsq.sfgpetclinic.model.Specialty;
+import de.qmsq.sfgpetclinic.repositories.SpecialtyRepository;
+import de.qmsq.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySdJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialitySdJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
